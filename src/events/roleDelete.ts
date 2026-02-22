8e07c288ec12c0e216872a793454e73a0aa83d6b
Cruck Store <cruckstore+github@gmail.com>
2026-02-22 18:33:39 +0100

    feat(events): handle Discord roleDelete event

--- src/events/roleDelete.ts
+++ src/events/roleDelete.ts
@@ -1,10 +1,13 @@
 import { Role } from "discord.js";
 import type { Event } from "../types/index.js";
 import { LogService } from "../services/LogService.js";
+import { logger } from "../utils/index.js";
 
 const event: Event<"roleDelete"> = {
   name: "roleDelete",
   async execute(client, role: Role) {
+    logger.event(`Role deleted: @${role.name} | ${role.guild.name}`);
+
     const logService = new LogService(client);
     await logService.logRoleDelete(role);
   },
