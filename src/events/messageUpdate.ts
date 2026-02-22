b265e57f3507268159024a840a097b9945458d46
Cruck Store <cruckstore+github@gmail.com>
2026-02-22 18:33:39 +0100

    feat(events): handle Discord messageUpdate event

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,11 +1,15 @@
 import { Message, PartialMessage } from "discord.js";
 import type { Event } from "../types/index.js";
 import { LogService } from "../services/LogService.js";
+import { logger } from "../utils/index.js";
 
 const event: Event<"messageUpdate"> = {
   name: "messageUpdate",
   async execute(client, oldMessage: Message | PartialMessage, newMessage: Message | PartialMessage) {
     if (!oldMessage.guild || oldMessage.partial || newMessage.partial) return;
+    if (oldMessage.content === newMessage.content) return;
+
+    logger.event(`Message edited: ${oldMessage.author?.tag} in #${oldMessage.channel && "name" in oldMessage.channel ? oldMessage.channel.name : oldMessage.channelId} | ${oldMessage.guild.name}`);
 
     const logService = new LogService(client);
     await logService.logMessageEdit(oldMessage as Message, newMessage as Message);
