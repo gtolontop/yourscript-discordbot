efb432332df877cbeec067c5dedfc20c59032882
Cruck Store <cruckstore+github@gmail.com>
2026-02-22 18:33:39 +0100

    feat(events): handle Discord roleUpdate event

--- src/events/roleUpdate.ts
+++ src/events/roleUpdate.ts
@@ -1,10 +1,17 @@
 import { Role } from "discord.js";
 import type { Event } from "../types/index.js";
 import { LogService } from "../services/LogService.js";
+import { logger } from "../utils/index.js";
 
 const event: Event<"roleUpdate"> = {
   name: "roleUpdate",
   async execute(client, oldRole: Role, newRole: Role) {
+    if (oldRole.name !== newRole.name) {
+      logger.event(`Role renamed: @${oldRole.name} -> @${newRole.name} | ${newRole.guild.name}`);
+    } else {
+      logger.event(`Role updated: @${newRole.name} | ${newRole.guild.name}`);
+    }
+
     const logService = new LogService(client);
     await logService.logRoleUpdate(oldRole, newRole);
   },
