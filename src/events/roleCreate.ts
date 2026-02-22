b87dad8664835c73a160f9a89e8d0adcc8f8d983
Cruck Store <cruckstore+github@gmail.com>
2026-02-22 18:33:39 +0100

    feat(events): handle Discord roleCreate event

--- src/events/roleCreate.ts
+++ src/events/roleCreate.ts
@@ -1,10 +1,13 @@
 import { Role } from "discord.js";
 import type { Event } from "../types/index.js";
 import { LogService } from "../services/LogService.js";
+import { logger } from "../utils/index.js";
 
 const event: Event<"roleCreate"> = {
   name: "roleCreate",
   async execute(client, role: Role) {
+    logger.event(`Role created: @${role.name} | ${role.guild.name}`);
+
     const logService = new LogService(client);
     await logService.logRoleCreate(role);
   },
