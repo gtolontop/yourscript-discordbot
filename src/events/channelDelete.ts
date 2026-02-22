ae6b013564bc738d4d66b70b5b6f5e48b4178f1e
Cruck Store <cruckstore+github@gmail.com>
2026-02-22 18:33:38 +0100

    feat(events): handle Discord channelDelete event

--- src/events/channelDelete.ts
+++ src/events/channelDelete.ts
@@ -1,11 +1,14 @@
 import { DMChannel, GuildChannel, NonThreadGuildBasedChannel } from "discord.js";
 import type { Event } from "../types/index.js";
 import { LogService } from "../services/LogService.js";
+import { logger } from "../utils/index.js";
 
 const event: Event<"channelDelete"> = {
   name: "channelDelete",
   async execute(client, channel: DMChannel | NonThreadGuildBasedChannel) {
     if (channel.isDMBased()) return;
+
+    logger.event(`Channel deleted: #${channel.name} | ${channel.guild.name}`);
 
     const logService = new LogService(client);
     await logService.logChannelDelete(channel as GuildChannel);
